93bb492f15b6a010cd0044b821ddc07206531768
lauraac <laura-a-c@hotmail.com>
2021-07-14 00:23:37 -0500

    start proyect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import data from './data.json';
+
+function App() {
+    console.log(data)
+    return ( < div className = "App" >
+        <
+        h1 > Hola mundos < /h1> <
+        /div>
+    );
+
+}
+
+export default App;
